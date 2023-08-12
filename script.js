0fd69f1d7a4bd56dcec167408ba4f0c46b59b2e3
joabe <joabe9999pereira@gmail.com>
2023-08-12 16:37:00 -0300

    language change

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 // 1 função que cria o titulo H1.
-function criaTitulo() {
-  const criaH1 = document.createElement('h1');
-  criaH1.innerHTML = 'Paleta de Cores.';
-  criaH1.id = 'title';
-  document.body.appendChild(criaH1);
+function createTitle() {
+  const createH1 = document.createElement('h1');
+  createH1.innerHTML = 'Paleta de Cores.';
+  createH1.id = 'title';
+  document.body.appendChild(createH1);
 }
-criaTitulo();
+createTitle();
+
 // 2 função que adiciona a paleta de cores.
-function adicionaPaletaDeCores() {
-  const criaSection = document.createElement('section');
-  criaSection.id = 'color-palette';
-  document.body.appendChild(criaSection);
+function addCollorPalette() {
+  const createSection = document.createElement('section');
+  createSection.id = 'color-palette';
+  document.body.appendChild(createSection);
 }
-adicionaPaletaDeCores();
+addCollorPalette();
+
 // 3 função que adiciona cores a paleta de cores.
-function adicionaAsCores() {
-  const maeDasDivs = document.querySelector('#color-palette');
+function addColors() {
+  const mainDiv = document.querySelector('#color-palette');
   for (let i = 0; i < 4; i += 1) {
-    const criaDivs = document.createElement('div');
-    maeDasDivs.appendChild(criaDivs);
-    criaDivs.className = 'color';
-    criaDivs.style.border = '1px solid black';
+    const divs = document.createElement('div');
+    mainDiv.appendChild(divs);
+    divs.className = 'color';
+    divs.style.border = '1px solid black';
     if (i === 0) {
-      criaDivs.id = 'black';
-      criaDivs.classList = 'color selected';
+      divs.id = 'black';
+      divs.classList = 'color selected';
     }
   }
 }
-adicionaAsCores();
+addColors();
+
 // 4 função que adiciona as cores selecionaveis a paleta de cores.
-function coresSelecionaveis() {
-  const preto = document.querySelector('#black');
-  preto.style.backgroundColor = 'black';
+function selectedColors() {
+  const blackColor = document.querySelector('#black');
+  blackColor.style.backgroundColor = 'black';
   const chartreuse = document.getElementsByTagName('div')[1];
   chartreuse.style.backgroundColor = 'chartreuse';
   const magenta = document.getElementsByTagName('div')[2];
@@ -39,64 +42,65 @@ function coresSelecionaveis() {
   const cyan = document.getElementsByTagName('div')[3];
   cyan.style.backgroundColor = 'cyan';
 }
-coresSelecionaveis();
+selectedColors();
+
 // 5 função que cria quadro de pixels (com outra função que cria os quadrados do pixel)
-function criaQuadradosDePixels() {
-  const quadro = document.createElement('section');
-  quadro.id = 'pixel-board';
-  quadro.style.width = '210px';
-  quadro.style.height = '210px';
-  document.body.appendChild(quadro);
-  function quadradosDePixel() {
-    for (let indice = 0; indice < 25; indice += 1) {
-      const quadrados = document.createElement('div');
-      quadrados.className = 'pixel';
-      quadrados.style.width = '40px';
-      quadro.appendChild(quadrados);
-      quadrados.style.backgroundColor = 'white';
+function createPixelSquares() {
+  const frame = document.createElement('section');
+  frame.id = 'pixel-board';
+  frame.style.width = '210px';
+  frame.style.height = '210px';
+  document.body.appendChild(frame);
+  function pixelSquares() {
+    for (let index = 0; index < 25; index += 1) {
+      const squares = document.createElement('div');
+      squares.className = 'pixel';
+      squares.style.width = '40px';
+      frame.appendChild(squares);
+      squares.style.backgroundColor = 'white';
     }
   }
-  quadradosDePixel();
+  pixelSquares();
 }
-criaQuadradosDePixels();
+createPixelSquares();
 
-const paletaDeCores = document.getElementById('color-palette');
-
+const colorsPalette = document.getElementById('color-palette');
 // 6 funçao que muda as cores.
-function mudaCores(cor) {
-  const corSelecionada = document.querySelector('.selected');
-  corSelecionada.className += 'selected';
-  cor.target.classList.add('selected');
+function changeColors(color) {
+  const selectedColor = document.querySelector('.selected');
+  selectedColor.className += 'selected';
+  color.target.classList.add('selected');
 }
-paletaDeCores.addEventListener('click', mudaCores);
+colorsPalette.addEventListener('click', changeColors);
 
 // 7 função que cria botão que muda as cores.
-const quadradoDePIxels1 = document.getElementById('pixel-board');
-function corDoQuadrado(corClicada) {
-  const corEscolhida = document.querySelector('.selected');
-  if (corClicada.target.className === 'pixel') {
-    corClicada.target.style.backgroundColor = corEscolhida.style.backgroundColor;
+const firstPixelSquares = document.getElementById('pixel-board');
+function squareColor(selectedColor) {
+  const chosenColor = document.querySelector('.selected');
+  if (selectedColor.target.className === 'pixel') {
+    selectedColor.target.style.backgroundColor = chosenColor.style.backgroundColor;
   }
 }
-quadradoDePIxels1.addEventListener('click', corDoQuadrado);
-// 8 função que cria um botão de limpar.
-function criaUmBotaoDeLimpar() {
-  const divMaeDoBotaoDeLimpar = document.createElement('div');
-  divMaeDoBotaoDeLimpar.className = 'div-Do-Limpa';
-  const botaoDeLimpar = document.createElement('button');
-  botaoDeLimpar.id = 'clear-board';
-  botaoDeLimpar.innerText = 'Limpar';
-  divMaeDoBotaoDeLimpar.appendChild(botaoDeLimpar);
-  quadradoDePIxels1.before(divMaeDoBotaoDeLimpar);
-}
-criaUmBotaoDeLimpar();
+firstPixelSquares.addEventListener('click', squareColor);
 
-const botaoQueLimpa = document.getElementById('clear-board');
+// 8 função que cria um botão de limpar.
+function CleanButton() {
+  const mainCleanDiv = document.createElement('div');
+  mainCleanDiv.className = 'div-Do-Limpa';
+  const CleanButtonDOM = document.createElement('button');
+  CleanButtonDOM.id = 'clear-board';
+  CleanButtonDOM.innerText = 'Limpar';
+  mainCleanDiv.appendChild(CleanButtonDOM);
+  firstPixelSquares.before(mainCleanDiv);
+}
+CleanButton();
+const cleaner = document.getElementById('clear-board');
+
 // 9 função que transforma o texto colorido em branco a partir do botão de limpar
-function fazALimpa() {
-  const quadradinhos = document.getElementsByClassName('pixel');
-  for (let i2 = 0; i2 < quadradinhos.length; i2 += 1) {
-    quadradinhos[i2].style.backgroundColor = 'white';
+function cleanPixels() {
+  const littleSquares = document.getElementsByClassName('pixel');
+  for (let i2 = 0; i2 < littleSquares.length; i2 += 1) {
+    littleSquares[i2].style.backgroundColor = 'white';
   }
 }
-botaoQueLimpa.addEventListener('click', fazALimpa);
+cleaner.addEventListener('click', cleanPixels);
